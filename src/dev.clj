92bdfe058aca7dec56c3c6411ee243ab68db1ba8
victorbjelkholm <victorbjelkholm@gmail.com>
2020-11-20 16:53:30 +0100

    Function for adding websites via dev.clj
    License: MIT
    Signed-off-by: Victor Bjelkholm <git@victor.earth>

--- src/dev.clj
+++ src/dev.clj
@@ -1,7 +1,9 @@
 (ns dev
   (:require
     [instant-website.db :as db]
-    [instant-website.core :as core]))
+    [instant-website.core :as core]
+    [cheshire.core :refer [parse-string generate-string]]
+    [clj-http.client :as http]))
 
 (defn new-user []
   (let [login-code (db/->login-code "test@example.com")
@@ -12,9 +14,22 @@
     {:user user
      :token token}))
 
+(defn create-website [token file]
+  (->
+    (http/post "http://localhost:8080/plugin-api/websites"
+               {:headers {"Authorization" (str "Token " token)}
+                :body (slurp (str "e2e/json-payloads/" file ".json"))})
+    :body
+    (parse-string true)
+    :crux.db/id))
+
 (comment
   (new-user)
   (def user (new-user))
+  (def figma-token (-> user :token :tokens/plugin))
+
+  (create-website figma-token "filled-container-auto-layout")
+
   (identity user)
   (db/put! @core/crux-node (:user user))
   (db/put! @core/crux-node (:token user)))
